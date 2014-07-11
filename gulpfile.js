cb499c87252c7daa91a3357d893645764d96c076
Jason Sanford <jasonsanford@gmail.com>
2014-07-11 16:41:59 -0400

    Set up testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var mocha = require('gulp-mocha');
 
 gulp.task('compile', function() {
   gulp.src(['./src/*.coffee', './src/**/*.coffee'])
@@ -8,8 +9,17 @@ gulp.task('compile', function() {
     .pipe(gulp.dest('./lib/'));
   });
 
+gulp.task('mocha', function () {
+  gulp.src('./test/*.js')
+    .pipe(
+      mocha({ reporter: 'list' })
+        .on('error', gutil.log)
+    );
+});
+
 gulp.task('watch', function() {
-  gulp.watch(['./src/*.coffee', './src/**/*.coffee'], ['compile']);
+  gulp.watch(['./src/*.coffee', './src/**/*.coffee'], ['compile', 'mocha']);
+  gulp.watch(['./test/*.js'], ['mocha']);
 });
 
 gulp.task('default', ['compile']);
