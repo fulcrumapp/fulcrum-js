0ac249bd8b9f617a074e49d235b5e34fc337a633
Jason Sanford <jasonsanford@gmail.com>
2014-07-16 15:06:15 -0400

    Forget GeoJSON

--- test/test_records.js
+++ test/test_records.js
@@ -7,15 +7,15 @@ var client = require('./client');
 describe('Record', function(){
 
   describe('#find()', function(){
-    it('should return a Feature.', function(done){
+    it('should return a record.', function(done){
       var records = nock('https://api.fulcrumapp.com')
                     .get('/api/v2/records/916474a7-b995-4b36-81db-8eda97f93a73')
                     .replyWithFile(200, __dirname + '/objects/record.json');
       client.record.find('916474a7-b995-4b36-81db-8eda97f93a73', function(error, record) {
         assert.ifError(error);
-        assert.equal(record.geometry.type, 'Point', 'geometry.type is not "Point".');
-        assert.equal(record.geometry.coordinates[0], -100, 'geometry.coordinates[0] is incorrect.');
-        assert.equal(record.geometry.coordinates[1], 40, 'geometry.coordinates[1] is incorrect.');
+        assert.equal(record.id, '916474a7-b995-4b36-81db-8eda97f93a73', 'record.id is incorrect.');
+        assert.equal(record.longitude, -100, 'geometry.coordinates[0] is incorrect.');
+        assert.equal(record.latitude, 40, 'geometry.coordinates[1] is incorrect.');
         done();
       });
     });
@@ -44,13 +44,13 @@ describe('Record', function(){
   });
 
   describe('#search()', function(){
-    it('should return a FeatureCollection.', function(done){
+    it('should return records.', function(done){
       var records = nock('https://api.fulcrumapp.com')
                     .get('/api/v2/records?form_id=512342b0-2bce-4e31-9d4a-8f29e929f7ac')
                     .replyWithFile(200, __dirname + '/objects/records.json');
       client.record.search({form_id: '512342b0-2bce-4e31-9d4a-8f29e929f7ac'}, function(error, records) {
         assert.ifError(error);
-        assert.equal(records.type, 'FeatureCollection', 'records is not a FeatureCollection.');
+        assert(records instanceof Array, 'records is not an array.');
         done();
       });
     });
@@ -64,7 +64,7 @@ describe('Record', function(){
                     .replyWithFile(201, __dirname + '/objects/record.json');
       client.record.create(record, function(error, record) {
         assert.ifError(error);
-        assert.equal(record.type, 'Feature', 'record is not a Feature.');
+        assert.equal(record.id, '916474a7-b995-4b36-81db-8eda97f93a73', 'record.id is incorrect.');
         done();
       });
     });
@@ -78,7 +78,7 @@ describe('Record', function(){
                     .replyWithFile(200, __dirname + '/objects/record.json');
       client.record.update('916474a7-b995-4b36-81db-8eda97f93a73', record, function(error, record) {
         assert.ifError(error);
-        assert.equal(record.type, 'Feature', 'record is not a Feature.');
+        assert.equal(record.id, '916474a7-b995-4b36-81db-8eda97f93a73', 'record.id is incorrect.');
         done();
       });
     });
