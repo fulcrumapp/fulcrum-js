ebb4f47af13d33e70b817af86a2a5170024f1f5f
Jason Sanford <jasonsanford@gmail.com>
2015-10-11 20:43:53 -0400

    Add changePermissions method to memberships.

--- test/test_memberships.js
+++ test/test_memberships.js
@@ -19,4 +19,25 @@ describe('memberships', function(){
     });
   });
 
+  describe('#changePermissions()', function(){
+    it('should change permissions.', function(done){
+      var permissions = {
+        "change":{
+          "type":"layer_members",
+          "layer_id":"68b2e0e3-ff36-460f-8f88-f8d3e70acbf2",
+          "remove":[
+            "00dd4fd1-1c09-4204-897e-ca68694f5d34"
+          ]
+        }
+      };
+      var nocker = nock('https://api.fulcrumapp.com')
+                   .post('/api/v2/memberships/change_permissions')
+                   .replyWithFile(200, __dirname + '/objects/memberships.json');
+      client.memberships.changePermissions(permissions, function(error, memberships) {
+        assert.ifError(error);
+        assert(memberships.memberships instanceof Array, 'memberships is not an array.');
+        done();
+      });
+    });
+  });
 });
