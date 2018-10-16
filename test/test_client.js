02f60125f93cc152f40cceb14c708355c65715d8
Jason Sanford <jasonsanford@gmail.com>
2018-10-16 16:36:33 -0400

    Add query tests

--- test/test_client.js
+++ test/test_client.js
@@ -5,7 +5,7 @@ import client from './client';
 
 describe('Client', () => {
   describe('.api instanceof', () => {
-    it('#api should be an instance of Fetcher', () => {
+    it('#api should be an instance of Fetcher.', () => {
       assert.ok(client.api instanceof Fetcher);
     });
   });
