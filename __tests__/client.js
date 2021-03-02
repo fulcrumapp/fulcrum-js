daee90827f634fcccdd9b6b1965fea6809bb5217
Elijah Dorman <edorman@spectrumsi.com>
2021-03-02 09:04:13 -0600

    Added client test
    Signed-off-by: Elijah Dorman <edorman@spectrumsi.com>

--- __tests__/client.js
+++ __tests__/client.js
@@ -2,4 +2,10 @@ import { Client } from "../dist";
 
 const client = new Client("fake");
 
+describe("client", () => {
+  it("should be an instance of Client class", () => {
+    expect(client instanceof Client).toBe(true);
+  });
+});
+
 export default client;
