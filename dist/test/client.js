70732485e127f424a89a5405c31b113f490a42e2
Zac McCormick <zac.mccormick@gmail.com>
2024-09-18 14:13:51 -0400

    Build output

--- dist/test/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("../src");
-const client = new src_1.Client('fake');
-exports.default = client;
-//# sourceMappingURL=client.js.map
